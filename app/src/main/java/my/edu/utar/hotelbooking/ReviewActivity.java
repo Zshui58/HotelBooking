d54b59e575253160c99e23d7b76777eb3ba493a0
Zshui58 <shu.hui.9809672@1utar.my>
2023-08-29 23:55:56 +0800

    review list

--- app/src/main/java/my/edu/utar/hotelbooking/ReviewActivity.java
+++ app/src/main/java/my/edu/utar/hotelbooking/ReviewActivity.java
@@ -1,14 +1,32 @@
 package my.edu.utar.hotelbooking;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ReviewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        recyclerView = findViewById(R.id.reviewRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Create sample review data
+        List<Review> reviews = new ArrayList<>();
+        reviews.add(new Review("User1","Jan 2023", "Great app!"));
+        reviews.add(new Review("User2","April 2023", "Needs improvements."));
+        // Add more reviews as needed
+
+        adapter = new ReviewAdapter(reviews);
+        recyclerView.setAdapter(adapter);
     }
 }
