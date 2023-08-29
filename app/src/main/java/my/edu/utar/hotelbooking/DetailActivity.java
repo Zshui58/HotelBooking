ee0f8a9d6996115cba0cebb792e0796d6ad1ed38
Zshui58 <shu.hui.9809672@1utar.my>
2023-08-29 22:24:11 +0800

    reviewpage

--- app/src/main/java/my/edu/utar/hotelbooking/DetailActivity.java
+++ app/src/main/java/my/edu/utar/hotelbooking/DetailActivity.java
@@ -2,7 +2,10 @@ package my.edu.utar.hotelbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView reviewTextView = findViewById(R.id.review);
+        reviewTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity here
+                Intent intent = new Intent(DetailActivity.this, ReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
