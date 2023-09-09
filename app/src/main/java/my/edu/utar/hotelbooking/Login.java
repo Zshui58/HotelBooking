b6fc31bc55ffb2fef593b5bc36345ed4a1d3fbe1
Babylegs <waichai2002@gmail.com>
2023-09-09 15:22:34 +0800

    Google Account

--- app/src/main/java/my/edu/utar/hotelbooking/Login.java
+++ app/src/main/java/my/edu/utar/hotelbooking/Login.java
@@ -11,13 +11,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Login extends AppCompatActivity {
     TextInputEditText editTextEmail,editTextPassword;
@@ -25,6 +35,12 @@ public class Login extends AppCompatActivity {
     FirebaseAuth auth;
     ProgressBar progressBar;
     TextView tv;
+
+    //Google
+    SignInButton googleAuth;
+    FirebaseDatabase database;
+    GoogleSignInClient googleSignInClient;
+    int RC_SIGN_IN = 20;
 
     @Override
     public void onStart(){
@@ -96,5 +112,75 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        googleAuth = findViewById(R.id.googleAuth);
+        database = FirebaseDatabase.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        googleAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
+    }
+
+    private void googleSignIn() {
+        // Sign out of any currently signed-in Google account
+        googleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                // After signing out, start the Google Sign-In process
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent, RC_SIGN_IN);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+            } catch (ApiException e) {
+                Toast.makeText(this, "ActivityResult Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+    private void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = auth.getCurrentUser();
+                            HashMap<String,Object> map = new HashMap<>();
+                            map.put("id",user.getUid());
+                            map.put("name",user.getDisplayName());
+                            map.put("profile",user.getPhotoUrl().toString());
+
+                            database.getReference().child("users").child(user.getUid()).setValue(map);
+                            Intent intent = new Intent(Login.this,MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(Login.this, "something error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
